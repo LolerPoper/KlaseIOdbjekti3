ab3e1622b60c047f845ef6e260e3b0bc417d85b2
LolerPoper <113007900+LolerPoper@users.noreply.github.com>
2022-11-09 08:57:22 +0100

    Add files via upload

---
+++ KlaseIObjekti3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlaseIObjekti3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stablo stablo1 = new Stablo(true);
+            Stablo stablo2 = new Stablo(false);
+
+            Console.WriteLine("Stablo 1: " + stablo1.ToString());
+            Console.WriteLine("Stablo 2: " + stablo2.ToString());
+
+            Console.ReadKey();
+        }
+        
+    }
+    class Stablo
+    {
+        bool odpadajuListovi;
+        string vrstaStabla;
+
+        public bool OdpadajuListovi { get => odpadajuListovi; set => odpadajuListovi = value; }
+
+        public override string ToString()
+        {
+            return vrstaStabla;
+        }
+
+        public Stablo()
+        {
+
+        }
+        public Stablo(bool odpadajuListovi)
+        {
+            this.odpadajuListovi = odpadajuListovi;
+
+            if (odpadajuListovi)
+            {
+                this.vrstaStabla = "Vrsta stabla je bijelogorično";
+            }
+            else
+            {
+                this.vrstaStabla = "Vrsta stabla je crnogorično";
+            }
+        }
+
+    }
+}
